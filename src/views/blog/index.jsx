b5995bdb404925c39616e62af96231284d564470
Anirudh Patel <anirudh26p@gmail.com>
2023-06-10 00:50:18 +0530

    Update index.jsx

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -1,7 +1,11 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <Box width={'90%'} margin={'1rem auto'}>
+      <Typography>Blog title</Typography>
+      
+    </Box>
   )
 }

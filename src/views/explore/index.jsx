b836edaeeab1beb2da35101c8dbc0b00ed913551
Anirudh Patel <anirudh26p@gmail.com>
2023-07-21 23:44:34 +0530

    explore page design changed

--- src/views/explore/index.jsx
+++ src/views/explore/index.jsx
@@ -2,12 +2,14 @@ import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfi
 import { Box, InputAdornment, TextField, Typography } from '@mui/material'
 import Axios from 'axios';
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function Explore() {
     const [users, setUsers] = React.useState([]);
     const navigate = useNavigate();
+    const mode = useSelector((state) => state.mode);
     const userSearch = (value) => {
         Axios.post(`${process.env.REACT_APP_API_URL}/auth/getUsers`, {
             keyword: value,
@@ -37,6 +39,7 @@ export default function Explore() {
                             </InputAdornment>
                         ),
                     }}
+                    color={mode === 'light' ? 'secondary' : 'primary'}
                     variant="outlined"
                     onChange={(e) => {
                         setTimeout(() => {
@@ -45,18 +48,32 @@ export default function Explore() {
                     }}
                 />
 
-                <Box width={'100%'} display='flex' flexWrap='wrap'>
+                <Box width={'100%'} margin={'2rem 0'} display='flex' flexDirection='column'>
                     {users.map((user) => {
-                        return(
-                            <Box display={'flex'} flexDirection={'column'} alignItems={'center'} minWidth={'200px'} width={'27%'} bgcolor={'black'} margin={'2rem auto'} borderRadius={'30px'} sx={{
+                        return (
+                            <Box display={'flex'} flexDirection={'row'} justifyContent={'space-around'} alignItems={'center'} width={'70%'} bgcolor={mode === 'light' ? 'white' : 'black'} border={mode === 'light' ? '1px solid black' : 'none'} padding={'1rem'} margin={'2rem auto'} sx={{
                                 transition: 'all 0.5s',
                                 '&:hover': {
-                                    padding: '1rem'
+                                    padding: '1.5rem'
+                                },
+                                '@media only screen and (max-width: 700px)': {
+                                    width: '80%',
+                                    margin: '1rem auto'
                                 }
-                            }} key={user._id}>
-                                <img src='/img/user-default-logo.png' style={{ margin: '2rem 0 1rem 0'}} alt='' width='40%'></img>
-                                <Typography color={'white'} fontSize={'35px'} marginBottom={'1rem'} onClick={() => {navigate(`/profile/${user.username}`)}}>{user.username}</Typography>
-                                <Typography color={'grey'} fontSize={'20px'} marginBottom={'1rem'}>{user.name}</Typography>
+                            }} key={user._id} onClick={() => { navigate(`/profile/${user.username}`) }}>
+                                <Box width={'30%'} display={'flex'} justifyContent={'center'} sx={{
+                                        '@media only screen and (max-width: 700px)': {
+                                            width: '50%'
+                                        }
+                                    }}>
+                                    <img src='/img/user-default-logo.png' alt='' width='30%'></img>
+                                </Box>
+                                <Typography color={mode === 'light' ? 'secondary' : 'primary'} width='35%' display='flex' justifyContent='center'>{user.username}</Typography>
+                                <Typography sx={{
+                                    '@media only screen and (max-width: 700px)': {
+                                        display: 'none'
+                                    }
+                                }} color={'grey'} width='35%' display='flex' justifyContent='center'>{user.name}</Typography>
                             </Box>
                         )
                     })}

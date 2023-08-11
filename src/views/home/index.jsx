ea31b7467150685125a07465d513e07a31f58dc3
Anirudh Patel <107875381+anirudhp26@users.noreply.github.com>
2023-08-11 18:02:04 +0530

    Update index.jsx

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,10 +3,8 @@ import { useSelector } from 'react-redux';
 import Login from '../login';
 import Navbar from '../../components/Navbar';
 import { Typography, Box, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const navigate = useNavigate();
     const isLoggedin = useSelector((state) => state.token);
     const ifloggedIn = () => {
         if (isLoggedin) {

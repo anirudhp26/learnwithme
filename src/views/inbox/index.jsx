31bca29496065e9ece404643965fe29dda262236
Anirudh Patel <anirudh26p@gmail.com>
2023-08-15 17:17:30 +0530

    Update index.jsx

--- src/views/inbox/index.jsx
+++ src/views/inbox/index.jsx
@@ -1,17 +1,10 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
-// import io from 'socket.io-client';
-
+import { Box, TextField } from '@mui/material';
+import React from 'react';
 
 export default function Inbox() {
-    // const socket = io.connect('http://localhost:3001');
-    // const handleSendMessage = async () => {
-    //     socket.emit("send_notification_follow", { message: "New user Impressed", impressed: "" })        
-    // }
   return (
     <Box display={'flex'} width={'70%'} margin={'2rem auto'}>
         <TextField variant='outlined' placeholder='send messages' fullWidth />
-        {/* <Button variant='outlined' onClick={() => {handleSendMessage()}}>Send msg</Button> */}
     </Box>
   )
 }

a906ebc09622e60d8fed81043391c3efaab3b566
Anirudh Patel <anirudh26p@gmail.com>
2023-08-31 06:27:28 +0530

    blog view working now

--- src/components/BlogView/index.jsx
+++ src/components/BlogView/index.jsx
@@ -9,27 +9,34 @@ export default function BlogView() {
     const { blogId } = useParams();
     const token = useSelector((state) => state.token);
     const theme = useTheme();
-    const [blog, setBlog] = useState({});
+    const [blog, setBlog] = useState(null);
     useEffect(() => {
-        const blog = axios.post(`${process.env.REACT_APP_API_URL}/blog/getBlogbyID`, { blogId: blogId }, { headers: { Authorization: `Bearer ${token}` } });
-        if (blog.status === 200) {
-            setBlog(blog.data.blog);
+        const getblog = async () => {
+            const blog = await axios.post(`${process.env.REACT_APP_API_URL}/blog/getBlogbyID`, { blogId: blogId }, { headers: { Authorization: `Bearer ${token}` } });
+            if (blog.status === 200) {
+                setBlog(blog.data.blog);
+            }
+            console.log(blog);
+            return blog;
         }
+        getblog();
     }, [blogId, token]);
-  return (
-    <Box>
-        {blog
-          ?
-          <img src={`${process.env.REACT_APP_API_URL}/assets/` + blog.coverPath} alt='Cover' style={{ width: '100%', height: 'auto', aspectRatio: '4/1', margin: '2rem 0' }}></img>
-          :
-          <></>
-        }
-        <Typography fontSize={theme.typography.h1} textAlign={'center'} margin={'2rem 0'}>
-          {blog.title}
-        </Typography>
-        <Box overflow={'hidden'} margin={'2rem auto'} width={'100%'}>
-          <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+    return (
+        <Box width={'70%'} margin={'auto'}>
+            {blog
+                ?
+                <>
+                    <img src={`${process.env.REACT_APP_API_URL}/assets/` + blog.coverPath} alt='Cover' style={{ width: '100%', height: 'auto', aspectRatio: '4/1', margin: '2rem 0' }}></img>
+                    <Typography fontSize={theme.typography.h1} textAlign={'center'} margin={'2rem 0'}>
+                        {blog.title}
+                    </Typography>
+                    <Box overflow={'hidden'} margin={'2rem auto'} width={'100%'}>
+                        <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+                    </Box>
+                </>
+                :
+                <></>
+            }
         </Box>
-    </Box>
-  )
+    )
 }

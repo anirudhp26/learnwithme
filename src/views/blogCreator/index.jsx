bb9dfd323c499a0862d2a456c66dc2d08955a444
Anirudh Patel <anirudh26p@gmail.com>
2023-09-01 23:47:43 +0530

    Update index.jsx

--- src/views/blogCreator/index.jsx
+++ src/views/blogCreator/index.jsx
@@ -21,7 +21,8 @@ var modules = {
       { align: [] }
     ],
     [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
-  ]
+  ],
+
 };
 
 var formats = [
@@ -393,13 +394,13 @@ export default function BlogCreator() {
               textTransform: "none",
             }}
             id="auth-btn-2"
-          // onClick={() => {
-          // 	setisClicked((prev) => !prev);
-          // 	handleEditProfileSubmit();
-          // 	document
-          // 		.getElementById("edit-loading")
-          // 		.classList.toggle("disable");
-          // }}
+            // onClick={() => {
+            // 	setisClicked((prev) => !prev);
+            // 	handleEditProfileSubmit();
+            // 	document
+            // 		.getElementById("edit-loading")
+            // 		.classList.toggle("disable");
+            // }}
             onClick={() => {
               onBlogPost();
             }}

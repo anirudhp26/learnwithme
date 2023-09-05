c1eab642da50b98214f61c6bd60c017e204281be
Anirudh Patel <anirudh26p@gmail.com>
2023-09-05 22:53:37 +0530

    .

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -1,66 +1,34 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Card, CardContent, Typography, useTheme } from '@mui/material';
+
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function Blog(props) {
   const navigate = useNavigate();
-  const user = props.user;
+  const theme = useTheme();
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <Box width={'100%'} display={'flex'} flexDirection={'row'} justifyContent={'space-around'} margin={'1rem auto'} sx={{
-      transition: 'all 0.5s',
-      cursor: 'pointer',
-      '&:hover': {
-        backgroundColor: 'black',
-        padding: '0.5rem 0',
-        color: "white",
-      },
-      '@media only screen and (max-width: 530px)': {
-        flexDirection: 'column'
-      },
-      border: '1px solid grey'
-    }}
-      onClick={() => {
-        navigate(`/blogs/${props.id}`)
-      }}
-    >
-      <Box width={'42%'} height={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{
-        '@media only screen and (max-width: 530px)': {
-          width: '90%',
-          margin: '0 auto'
-        }
-      }}>
-        <Box width={'100%'} display='flex' flexDirection='row' alignItems='center' margin='1rem auto' sx={{
-          '@media only screen and (max-width: 530px)': {
-            flexDirection: 'column',
-            justifyContent: 'center',
-          }
-        }}>
-          <img src='/img/user-default-logo.png' alt='' width='50px'></img>
-          <Box margin='2rem 0 2rem 2rem' textAlign={'center'} sx={{
-            '@media only screen and (max-width: 530px)': {
-              width: '90%',
-              margin: 'auto',
-            }
-          }}>
-            <Typography sx={{ fontSize: '1.5rem' }}>{user.name}</Typography>
-            <Typography sx={{ fontSize: '1rem', marginTop: '0.5rem' }} color='#757575'>{user.username}</Typography>
-          </Box>
-        </Box>
-      </Box>
-      <Box width={'50%'} display={'flex'} alignItems={'center'}
-         sx={{
-          '@media only screen and (max-width: 530px)': {
-            width: '90%',
-            margin: 'auto',
-          }
-        }}
-      >
-        <Typography fontSize={'1.5rem'} sx={{
-            '@media only screen and (max-width: 530px)': {
-              margin: '1rem auto',
-            }
-          }}>{props.title}</Typography>
-      </Box>
-    </Box>
-  )
+    <Card sx={{ margin: '1rem'}}>
+      <CardContent style={{ position: 'relative' }}>
+        <div className={`glass-effect ${expanded ? 'expanded' : ''}`}>
+          <Typography fontSize={theme.typography.h2} gutterBottom>
+            {props.title}
+          </Typography>
+          <Typography fontSize={theme.typography.h4} color="textSecondary">
+            <div dangerouslySetInnerHTML={{ __html: expanded ? props.content : props.content.slice(0, 100)}}></div>
+          </Typography>
+          {!expanded && (
+            <Button onClick={toggleExpand} color="primary">
+              Read More
+            </Button>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-import { Typography, Box, Button } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import axios from 'axios';
+import Blog from '../blog';
 
 const Home = () => {
+    const [feed, setFeed] = useState([]);
     const isLoggedin = useSelector((state) => state.token);
     const user = useSelector((state) => state.user);
-    const handleRecommendations = async () => {
-        const recommendations = await axios.post(`${process.env.REACT_APP_API_URL}/blog/getRecommendations`, { id: user._id }, { headers: { Authorization: `Bearer ${isLoggedin}`}});
-        console.log(recommendations);
-    }
-
-    return(
+    useEffect(() => {
+        axios.post(`${process.env.REACT_APP_API_URL}/blog/getRecommendations`, { id: user._id }, { headers: { Authorization: `Bearer ${isLoggedin}` } }).then((recommendations) => {
+            setFeed(recommendations.data.blogs);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, [user, isLoggedin]);
+    return (
         <>
-                    <Box width={'90%'} margin={'1rem auto'}>
-                        <Typography>Home page</Typography>
-                        <Button onClick={() => { handleRecommendations() }}>Get recommendations</Button>
-                    </Box>
+            <Box width={'90%'} margin={'1rem auto'}>
+                <Typography>Home page</Typography>
+                {feed.map((blog) => {
+                    return (
+                        <Blog
+                            title={blog.title}
+                            content={blog.content}
+                            key={blog._id}
+                            id={blog._id}
+                        />
+                    );
+                })}
+            </Box>
         </>
     )
 }

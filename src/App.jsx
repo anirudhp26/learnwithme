6aa9a71c205172ec426a4cbb9bf0b60866ab13ae
anirudhp26 <anirudh26p@gmail.com>
2024-11-12 12:41:23 +0530

    assignment help pr

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,81 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Home from './views/home';
-import Profile from './views/profile';
-import Explore from './views/explore';
-import Pricing from './views/pricing';
-import Layout from './views/Layout';
-// import Blog from './views/blog';
-import EditProfile from './views/editProfile';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { createTheme } from '@mui/material/styles';
-import { themeSettings } from './theme';
-import Inbox from './views/inbox';
-import { SocketContext, socket } from './context/SocketContext';
-import BlogCreator from './views/blogCreator';
-import BlogView from './components/BlogView';
-import Login from './views/login';
-import VerifyPhoneorEmail from './views/VerifyPnE';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./views/home";
+import Profile from "./views/profile";
+import Explore from "./views/explore";
+import Pricing from "./views/pricing";
+import Layout from "./views/Layout";
+import EditProfile from "./views/editProfile";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Inbox from "./views/inbox";
+import { SocketContext, socket } from "./context/SocketContext";
+import BlogCreator from "./views/blogCreator";
+import BlogView from "./components/BlogView";
+import Login from "./views/login";
+import VerifyPhoneorEmail from "./views/VerifyPnE";
 function App() {
-  const mode = useSelector((state) => state.mode);
-  const token = useSelector((state) => state.token);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	const mode = useSelector((state) => state.mode);
+	const token = useSelector((state) => state.token);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <SocketContext.Provider value={socket}>
-          <Routes>
-            <Route element={<Login />} path='/login'></Route>
-            <Route element={<VerifyPhoneorEmail />} path='/verifyPnE'></Route>
-            <Route element={token ? <BlogCreator /> : <Navigate to={'/login'} />} path='/blog'></Route>
-            <Route element={<Layout />}>
-              <Route element={<Home />} path='/' index></Route>
-              <Route element={<Home />} path='/home' index></Route>
-              <Route element={<Explore />} path='/explore'></Route>
-              <Route element={<Pricing />} path='/pricing'></Route>
-              <Route element={<Profile />} path='/profile/:user'></Route>
-              <Route element={<BlogView />} path='/blogs/:blogId'></Route>
-              <Route element={<EditProfile />} path='/editprofile'></Route>
-              <Route element={<Inbox />} path='/inbox'></Route>
-            </Route>
-          </Routes>
-        </SocketContext.Provider>
-      </ThemeProvider>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<SocketContext.Provider value={socket}>
+					<Routes>
+						<Route element={<Login />} path="/login"></Route>
+						<Route
+							element={<VerifyPhoneorEmail />}
+							path="/verifyPnE"
+						></Route>
+						<Route
+							element={
+								token ? (
+									<BlogCreator />
+								) : (
+									<Navigate to={"/login"} />
+								)
+							}
+							path="/blog"
+						></Route>
+						<Route element={<Layout />}>
+							<Route element={<Home />} path="/" index></Route>
+							<Route
+								element={<Home />}
+								path="/home"
+								index
+							></Route>
+							<Route
+								element={<Explore />}
+								path="/explore"
+							></Route>
+							<Route
+								element={<Pricing />}
+								path="/pricing"
+							></Route>
+							<Route
+								element={<Profile />}
+								path="/profile/:user"
+							></Route>
+							<Route
+								element={<BlogView />}
+								path="/blogs/:blogId"
+							></Route>
+							<Route
+								element={<EditProfile />}
+								path="/editprofile"
+							></Route>
+							<Route element={<Inbox />} path="/inbox"></Route>
+						</Route>
+					</Routes>
+				</SocketContext.Provider>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/BlogCards/index.jsx
+++ src/components/BlogCards/index.jsx
@@ -1,112 +1,178 @@
-import { MoreVertOutlined } from '@mui/icons-material';
-import { Box, Button, Card, CardContent, IconButton, Menu, MenuItem, Typography, useTheme } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import axios from 'axios';
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { MoreVertOutlined } from "@mui/icons-material";
+import {
+	Box,
+	Button,
+	Card,
+	CardContent,
+	IconButton,
+	Menu,
+	MenuItem,
+	Typography,
+	useTheme,
+} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import axios from "axios";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Blog(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const user = props.user;
-  const token = useSelector((state) => state.token);
-  const isOwner = props.user._id === useSelector((state) => state.user._id) ? true : false;
-  const calculateAgeOfBlog = (createdAt) => {
-    const currentDate = new Date();
-    const blogDate = new Date(createdAt);
-    const timeDiff = currentDate - blogDate;
-    const daysAgo = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    return `${daysAgo} days ago`;
-  };
-  const handleBlogDelete = async () => {
-    const deleteBlog = await axios.post(`${process.env.REACT_APP_API_URL}/blog/deleteblog`, { id: props.id }, { headers: { Authorization: `Bearer ${token}` } });
-    if (deleteBlog.status === 200) {
-      navigate(`/profile/${user.username}`);
-    }
-  }
-  return (
-    <Card sx={{ margin: '1rem 0', border: `1px solid ${theme.palette.neutral.light}`, boxShadow: 'none', backgroundColor: 'transparent' }}>
-      <CardContent style={{ position: 'relative' }}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'} width={'100%'} sx={{
-            // eslint-disable-next-line no-useless-concat
-            backgroundImage: `url("` + `${props.coverPath}")`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}>
-            <Box display={'flex'} width={'max-content'} alignItems={'center'} sx={{
-              transition: 'all 0.5s',
-              border: `1px solid ${theme.palette.neutral.medium}`,
-              backgroundColor: theme.palette.background.default,
-              margin: '1rem',
-              padding: '1rem',
-              '&:hover': {
-                border: `1px solid ${theme.palette.neutral.mediumMain}`,
-              },
-            }}>
-              <Avatar alt={user.username} src={user.picture} style={{ marginRight: '1rem' }} />
-              <Typography fontSize={theme.typography.h6} color="textPrimary">
-                {user.username}
-              </Typography>
-            </Box>
-          </Box>
-        </div>
-        <Typography fontSize={theme.typography.h2} margin={'1rem 0'} textAlign={'center'}>
-          {props.title}
-        </Typography>
-        <div style={{
-          fontSize: theme.typography.h4,
-          textAlign: 'center',
-          margin: '2rem 0',
-          color: theme.palette.neutral.dark
-        }} dangerouslySetInnerHTML={{ __html: (props.content.slice(0, 150) + "...") }}></div>
-        <Button onClick={() => navigate(`/blogs/${props.id}`)} color="primary">
-          Read More
-        </Button>
-        <Typography textAlign={'end'} fontSize={theme.typography.caption} color="textSecondary">
-          {props.views} views &bull; {calculateAgeOfBlog(props.createdAt)} &bull; {props.impressed} Likes &nbsp;
-          {isOwner ?
-            <>
-              <IconButton
-                aria-label="more"
-                id="long-button"
-                aria-controls={open ? 'long-menu' : undefined}
-                aria-expanded={open ? 'true' : undefined}
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MoreVertOutlined />
-              </IconButton>
-              <Menu
-                id="long-menu"
-                MenuListProps={{
-                  'aria-labelledby': 'long-button',
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-              >
-                {/* <MenuItem onClick={handleClose}>
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	const navigate = useNavigate();
+	const theme = useTheme();
+	const user = props.user;
+	const token = useSelector((state) => state.token);
+	const isOwner =
+		props.user._id === useSelector((state) => state.user._id)
+			? true
+			: false;
+	const calculateAgeOfBlog = (createdAt) => {
+		const currentDate = new Date();
+		const blogDate = new Date(createdAt);
+		const timeDiff = currentDate - blogDate;
+		const daysAgo = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+		return `${daysAgo} days ago`;
+	};
+	const handleBlogDelete = async () => {
+		const deleteBlog = await axios.post(
+			`${process.env.REACT_APP_API_URL}/blog/deleteblog`,
+			{ id: props.id },
+			{ headers: { Authorization: `Bearer ${token}` } }
+		);
+		if (deleteBlog.status === 200) {
+			navigate(`/profile/${user.username}`);
+		}
+	};
+	return (
+		<Card
+			sx={{
+				margin: "1rem 0",
+				border: `1px solid ${theme.palette.neutral.light}`,
+				boxShadow: "none",
+				backgroundColor: "transparent",
+			}}
+		>
+			<CardContent style={{ position: "relative" }}>
+				<div style={{ display: "flex", alignItems: "center" }}>
+					<Box
+						display={"flex"}
+						flexDirection={"column"}
+						justifyContent={"space-between"}
+						width={"100%"}
+						sx={{
+							// eslint-disable-next-line no-useless-concat
+							backgroundImage: `url("` + `${props.coverPath}")`,
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+						}}
+					>
+						<Box
+							display={"flex"}
+							width={"max-content"}
+							alignItems={"center"}
+							sx={{
+								transition: "all 0.5s",
+								border: `1px solid ${theme.palette.neutral.medium}`,
+								backgroundColor:
+									theme.palette.background.default,
+								margin: "1rem",
+								padding: "1rem",
+								"&:hover": {
+									border: `1px solid ${theme.palette.neutral.mediumMain}`,
+								},
+							}}
+						>
+							<Avatar
+								alt={user.username}
+								src={user.picture}
+								style={{ marginRight: "1rem" }}
+							/>
+							<Typography
+								fontSize={theme.typography.h6}
+								color="textPrimary"
+							>
+								{user.username}
+							</Typography>
+						</Box>
+					</Box>
+				</div>
+				<Typography
+					fontSize={theme.typography.h2}
+					margin={"1rem 0"}
+					textAlign={"center"}
+				>
+					{props.title}
+				</Typography>
+				<div
+					style={{
+						fontSize: theme.typography.h4,
+						textAlign: "center",
+						margin: "2rem 0",
+						color: theme.palette.neutral.dark,
+					}}
+					dangerouslySetInnerHTML={{
+						__html: props.content.slice(0, 150) + "...",
+					}}
+				></div>
+				<Button
+					onClick={() => navigate(`/blogs/${props.id}`)}
+					color="primary"
+				>
+					Read More
+				</Button>
+				<Typography
+					textAlign={"end"}
+					fontSize={theme.typography.caption}
+					color="textSecondary"
+				>
+					{props.views} views &bull;{" "}
+					{calculateAgeOfBlog(props.createdAt)} &bull;{" "}
+					{props.impressed} Likes &nbsp;
+					{isOwner ? (
+						<>
+							<IconButton
+								aria-label="more"
+								id="long-button"
+								aria-controls={open ? "long-menu" : undefined}
+								aria-expanded={open ? "true" : undefined}
+								aria-haspopup="true"
+								onClick={handleClick}
+							>
+								<MoreVertOutlined />
+							</IconButton>
+							<Menu
+								id="long-menu"
+								MenuListProps={{
+									"aria-labelledby": "long-button",
+								}}
+								anchorEl={anchorEl}
+								open={open}
+								onClose={handleClose}
+							>
+								{/* <MenuItem onClick={handleClose}>
               Edit
             </MenuItem> */}
-                <MenuItem onClick={() => { handleBlogDelete() }}>
-                  Delete
-                </MenuItem>
-              </Menu>
-            </>
-            :
-            <></>}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+								<MenuItem
+									onClick={() => {
+										handleBlogDelete();
+									}}
+								>
+									Delete
+								</MenuItem>
+							</Menu>
+						</>
+					) : (
+						<></>
+					)}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
 }
